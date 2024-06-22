cdc74ff913192e641ae366bf2e6be5459a6d695e
Ruvaid Ahmad <118987852+Ruvaidahmad123@users.noreply.github.com>
2024-06-22 14:09:05 +0530

    Update Floor.java

--- Floor{Upper Bound}.java
+++ Floor{Upper Bound}.java
@@ -4,7 +4,7 @@ Examples
 
 Input: n = 7, x = 0 arr[] = {1,2,8,10,11,12,19}
 Output: -1
-Explanation: No element less than 0 is found. So output is "-1".
+Explanation: No element less than 0 is found. So output is "-1"
 Input: n = 7, x = 5 arr[] = {1,2,8,10,11,12,19}
 Output: 1
 Explanation: Largest Number less than 5 is 2 (i.e k = 2), whose index is 1(0-based indexing).*/
